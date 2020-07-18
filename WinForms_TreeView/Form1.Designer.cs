ca781cbf170fc5de0cad18e74540743d00b1793e
dsamoilenko <48868330+dsamoilenko@users.noreply.github.com>
2020-07-18 12:22:46 +0300

    Add files via upload

---
+++ WinForms_TreeView/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace WinForms_TreeView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Boeing");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Airbus");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Ту-154");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Ту-134");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Ту-144");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Ту", new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode4,
+            treeNode5});
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Ан");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("76");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("62");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Ил", new System.Windows.Forms.TreeNode[] {
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Самолёты", 0, 1, new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode6,
+            treeNode7,
+            treeNode10});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(220, 71);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Node1";
+            treeNode1.Text = "Boeing";
+            treeNode1.ToolTipText = "Обалденный самолёт";
+            treeNode2.Name = "Node2";
+            treeNode2.Text = "Airbus";
+            treeNode3.Name = "Node0";
+            treeNode3.Text = "Ту-154";
+            treeNode4.Name = "Node1";
+            treeNode4.Text = "Ту-134";
+            treeNode5.Name = "Node2";
+            treeNode5.Text = "Ту-144";
+            treeNode6.Name = "Node3";
+            treeNode6.Text = "Ту";
+            treeNode7.Name = "Node4";
+            treeNode7.Text = "Ан";
+            treeNode8.Name = "Node0";
+            treeNode8.Text = "76";
+            treeNode9.Name = "Node1";
+            treeNode9.Text = "62";
+            treeNode10.Name = "Node5";
+            treeNode10.Text = "Ил";
+            treeNode11.BackColor = System.Drawing.Color.Red;
+            treeNode11.ImageIndex = 0;
+            treeNode11.Name = "Node0";
+            treeNode11.SelectedImageIndex = 1;
+            treeNode11.Text = "Самолёты";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode11});
+            this.treeView1.Size = new System.Drawing.Size(396, 325);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.BeforeLabelEdit += new System.Windows.Forms.NodeLabelEditEventHandler(this.treeView1_BeforeLabelEdit);
+            this.treeView1.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.treeView1_BeforeExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(198, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать узлы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 67);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Создать узел";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 96);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Вставить узел";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 125);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Удалить узел";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 154);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(198, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Развернуть все ветки";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 183);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(198, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Свернуть все ветки";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(12, 212);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(198, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Сортировать";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(216, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 9;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 241);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(198, 23);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "Удалить всё";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 270);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(136, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Установить checkbox";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 293);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(202, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Разрешить редактирование меток";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(12, 316);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(122, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "Показавыть линии";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(12, 339);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(136, 17);
+            this.checkBox4.TabIndex = 14;
+            this.checkBox4.Text = "Показывать плюсики";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 362);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(146, 17);
+            this.checkBox5.TabIndex = 15;
+            this.checkBox5.Text = "Показывать подсказки";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(216, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(396, 20);
+            this.textBox2.TabIndex = 16;
+            this.textBox2.Text = "\\";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 408);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ WinForms_TreeView/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinForms_TreeView
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // загрузка картинок в дерево
+            Bitmap img1 = new Bitmap("Image1.bmp");
+            Bitmap img2 = new Bitmap("Image2.bmp");
+            treeView1.ImageList = new ImageList();
+            treeView1.ImageList.Images.Add(img1);
+            treeView1.ImageList.Images.Add(img2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                treeView1.Nodes.Add("Легковые автомобили");
+
+                // Конструктор TreeNode принимает Название, номер картинки для невыделенного состояния и для выделенного
+                treeView1.Nodes[1].Nodes.Add(new TreeNode("Honda", 0, 1));
+                treeView1.Nodes[1].Nodes[0].Nodes.Add(new TreeNode("Accord", 0, 1));
+                treeView1.Nodes[1].Nodes[0].Nodes.Add(new TreeNode("Civic", 0, 1));
+                treeView1.Nodes[1].Nodes[0].Nodes.Add(new TreeNode("Acura", 0, 1));
+                treeView1.Nodes[1].Nodes.Add(new TreeNode("Opel", 0, 1));
+                treeView1.Nodes[1].Nodes[1].Nodes.Add(new TreeNode("Cadet", 0, 1));
+                treeView1.Nodes[1].Nodes[1].Nodes.Add(new TreeNode("Omega", 0, 1));
+                treeView1.Nodes[1].Nodes[1].Nodes.Add(new TreeNode("Corsa", 0, 1));
+                treeView1.Nodes[1].Nodes.Add(new TreeNode("BMW", 0, 1));
+                treeView1.Nodes[1].Nodes[2].Nodes.Add(new TreeNode("Z8", 0, 1));
+                treeView1.Nodes[1].Nodes[2].Nodes.Add(new TreeNode("M5", 0, 1));
+                treeView1.Nodes[1].Nodes[2].Nodes.Add(new TreeNode("X5", 0, 1));
+
+                treeView1.Nodes.Add("Грузовые автомобили");
+                treeView1.Nodes[2].Nodes.Add(new TreeNode("КамАЗ", 0, 1));
+                treeView1.Nodes[2].Nodes.Add(new TreeNode("ЗИЛ", 0, 1));
+                treeView1.Nodes[2].Nodes.Add(new TreeNode("БелАЗ", 0, 1));
+                treeView1.Nodes[2].Nodes.Add(new TreeNode("Mercedes", 0, 1));
+            }
+            catch
+            {
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Если выделен какой-либо узел
+            if (treeView1.SelectedNode != null)
+            {
+                // Создать новый узел
+                TreeNode node = new TreeNode(textBox1.Text, 0, 1);
+                node.ToolTipText = "Новый узел";
+
+                // Добавить дочерний узел к выделенному элементу
+                treeView1.SelectedNode.Nodes.Add(node);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Если есть выделенный элемент и у него есть родительский элемент
+            if (treeView1.SelectedNode != null && treeView1.SelectedNode.Parent != null)
+            {
+                // Создать новый узел
+                TreeNode node = new TreeNode(textBox1.Text);
+
+                // Получить индекс выделенного элемента
+                int index = treeView1.SelectedNode.Index;
+
+                // Вставить новый узел перед выделенным элементом
+                treeView1.SelectedNode.Parent.Nodes.Insert(index, node);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Если есть выделенный элемент
+            if (treeView1.SelectedNode != null)
+            {
+                treeView1.SelectedNode.Remove();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // развернуть все ветки дерева
+            treeView1.ExpandAll();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // свернуть все ветки дерева
+            treeView1.CollapseAll();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            // подключение компаратора
+            treeView1.TreeViewNodeSorter = new Sorter();
+
+            // произвести сортировку дерева
+            treeView1.Sort();
+        }
+
+        /// <summary>
+        /// Вызывается после выделения узла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            // Обновить разделитель в строке полного пути к узлу
+            treeView1.PathSeparator = textBox2.Text;
+
+            // Отобразить полный путь к выделенному узлу
+            label1.Text = treeView1.SelectedNode.FullPath;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            // удалить все узлы дерева
+            treeView1.Nodes.Clear();
+            label1.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            treeView1.CheckBoxes = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            treeView1.LabelEdit = checkBox2.Checked;
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            treeView1.ShowLines = checkBox3.Checked;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            treeView1.ShowPlusMinus = checkBox4.Checked;
+        }
+
+        private void checkBox5_CheckedChanged(object sender, EventArgs e)
+        {
+            treeView1.ShowNodeToolTips = checkBox5.Checked;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// Запускается ДО разворачивания узла
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void treeView1_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            //MessageBox.Show("Before expand");
+        }
+
+        private void treeView1_BeforeLabelEdit(object sender, NodeLabelEditEventArgs e)
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// Сортирующий узлы дерева класс
+    /// </summary>
+    public class Sorter : IComparer
+    {
+        public int Compare(object obj1, object obj2)
+        {
+            string str = ((TreeNode)obj1).Text;
+            string str2 = ((TreeNode)obj2).Text;
+            return str.Length - str2.Length;
+        }
+    }
+}
